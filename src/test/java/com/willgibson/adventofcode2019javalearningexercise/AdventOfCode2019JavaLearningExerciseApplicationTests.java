6752b9e45e8ebfc021b8e4639205884794b36a17
Will Gibson <will.gibson@walterego.co.uk>
2020-10-13 15:08:48 +0100

    Initialise Spring Boot project

---
+++ src/test/java/com/willgibson/adventofcode2019javalearningexercise/AdventOfCode2019JavaLearningExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willgibson.adventofcode2019javalearningexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventOfCode2019JavaLearningExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
