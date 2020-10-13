b5f1cc8b3b33843c839e69e161455a1da74f4df1
Will Gibson <will.gibson@walterego.co.uk>
2020-10-13 15:35:57 +0100

    fuelCounterUpper_With12_Returns2

---
+++ src/main/java/com/willgibson/adventofcode2019javalearningexercise/day01/FuelCounterUpper.java
@@ -0,0 +1,16 @@
+package com.willgibson.adventofcode2019javalearningexercise.day01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelCounterUpper {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuelCounterUpper.class, args);
+    }
+
+    public int calculate(int[] stars) {
+        return 2;
+    }
+}
---
+++ src/test/java/com/willgibson/adventofcode2019javalearningexercise/day01/FuelCounterUpperTests.java
@@ -0,0 +1,23 @@
+package com.willgibson.adventofcode2019javalearningexercise.day01;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FuelCounterUpperTests {
+
+    private FuelCounterUpper fuelCounterUpper;
+
+    @BeforeEach
+    void initUseCase() {
+        fuelCounterUpper = new FuelCounterUpper();
+    }
+
+    @Test
+    void fuelCounterUpper_With12_Returns2() {
+        int[] stars = {12};
+        assertThat(fuelCounterUpper.calculate(stars)).isEqualTo(2);
+    }
+
+}
