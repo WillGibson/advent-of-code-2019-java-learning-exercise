7df602785a74c77090326f97c1175a73b8f8688f
Will Gibson <will.gibson@walterego.co.uk>
2020-10-16 17:15:23 +0100

    Correct answer coming out

--- src/main/java/com/willgibson/adventofcode2019/main/Main.java
+++ src/main/java/com/willgibson/adventofcode2019/main/Main.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @SpringBootApplication
 public class Main implements CommandLineRunner {
@@ -23,17 +24,22 @@ public class Main implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws FileNotFoundException {
-        FromTextFile fromTextFile = new FromTextFile();
+    public void run(String... args) throws Exception {
+        final FromTextFile fromTextFile = new FromTextFile();
 
-        FuelCounterUpperPart1 fuelCounterUpperPart1 = new FuelCounterUpperPart1();
-        FuelCounterUpperPart2 fuelCounterUpperPart2 = new FuelCounterUpperPart2();
-        int[] stars = fromTextFile.integerPerLineToArray(
-            Config.pathToBasePackage + "day01/input.txt"
-        );
+        final FuelCounterUpperPart1 fuelCounterUpperPart1 = new FuelCounterUpperPart1();
+        final FuelCounterUpperPart2 fuelCounterUpperPart2 = new FuelCounterUpperPart2();
+        final int[] stars = fromTextFile.integerPerLineToArray(Config.pathToBasePackage + "day01/input.txt");
         log.info("Day 01, Part 1 - The fuel required is " + fuelCounterUpperPart1.calculate(stars));
         log.info("Day 01, Part 2 - The fuel required is " + fuelCounterUpperPart2.calculate(stars));
 
-        IntCodeComputerPart1 intCodeComputer1 = new IntCodeComputerPart1();
+        final IntCodeComputerPart1 intCodeComputer1 = new IntCodeComputerPart1();
+        final String commandString = fromTextFile.contentToString(Config.pathToBasePackage + "day02/input.txt");
+        // Restore commandString to "1202 program alarm" state - l'il bit manual...
+        final String[] commandsArray = commandString.split(",");
+        commandsArray[1] = "12";
+        commandsArray[2] = "2";
+        final String restoredCommands = String.join(",", commandsArray);
+        log.info("Day 02, Part 1 - The value at position 0 is " + intCodeComputer1.run(restoredCommands).split(",")[0]);
     }
 }
