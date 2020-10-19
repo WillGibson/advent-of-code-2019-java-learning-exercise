a1e338ff0337091ff1c4ee004517f2ab322661b3
Will Gibson <will.gibson@walterego.co.uk>
2020-10-19 09:24:36 +0100

    Self CR

--- src/main/java/com/willgibson/adventofcode2019/main/Main.java
+++ src/main/java/com/willgibson/adventofcode2019/main/Main.java
@@ -11,9 +11,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 @SpringBootApplication
 public class Main implements CommandLineRunner {
 
